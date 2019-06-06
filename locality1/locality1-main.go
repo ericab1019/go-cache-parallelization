5008e2f0a5ede983eef72c49d5deba16afd10771
unknown <ericab1019@gmail.com>
2019-06-06 09:34:02 -0500

    adds second locality

--- locality1/locality1-main.go
+++ locality1/locality1-main.go
@@ -6,35 +6,37 @@ import (
 	"math/rand"
 )
 
-//with blocking by blocksize runs in about 10s, without blocksize lose spatial locality and takes 17s
+//with blocking by blocksize runs in about 7.5s, without blocksize lose spatial locality and takes 12s
 func main() {
-	blocksize:=8
+	// blocksize:=8
 	start:=time.Now()
 	
-	loopsize := 100000
+	loopsize := 40000
 	
 	var intArr []int
 	
 	var readArr []int
 	
-	for i:=0;i<loopsize; i++ {
+	for i:=0; i<loopsize; i++ {
 		intArr=append(intArr, 0)
 	}
-	for i:=0;i<loopsize; i++ {
+	for i:=0; i<loopsize; i++ {
 		readArr=append(readArr, rand.Intn(50))
 	}
 	
-	for j2:=0; j2<loopsize; j2=j2+blocksize {
+	// for j2:=0; j2<loopsize; j2=j2+blocksize {
 		
-		for i:=0; i<loopsize; i++ {
-			
-			//for j:=0; j<loopsize; j++ {
-			
-			for j:=j2; j<j2+blocksize; j++ {
-				intArr[i] += i+readArr[j]
-			}
+	for i:=0; i<loopsize; i++ {
+		
+		for j:=0; j<loopsize; j++ {
+			intArr[i] += i+readArr[j]
 		}
 	}
+		// 	for j:=j2; j<j2+blocksize; j++ {
+		// 		intArr[i] += i+readArr[j]	
+		// 	}
+		// }
+	//}
 	t:=time.Now()
 	fmt.Println(t.Sub(start));
 }
---
+++ locality2/locality2-main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"math/rand"
+)
+
+//if run with j first, takes 5 seconds on average on benchmark computer. Run with i first, takes 1 second. 
+func main() {
+	start:=time.Now()
+	
+	const loopsize = 10000
+	
+	var intArr [loopsize][loopsize]int
+	
+	var readArr []int
+	
+	for i:=0;i<loopsize; i++ {
+		readArr=append(readArr, rand.Intn(50))
+	}
+		
+	for i:=0; i<loopsize; i++ {
+		
+		for j:=0; j<loopsize; j++ {
+			intArr[i][j] += i+readArr[j]
+		}
+	}
+	t:=time.Now()
+	fmt.Println(t.Sub(start));
+}
+
